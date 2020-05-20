ed1946c57ed223d7f9f0ae971c56a9cdde19867d
Fernando Rodríguez-Izquierdo Serrano <fernando.rodriguez.izquierdo.serrano@everis.com>
2020-05-20 21:25:21 +0200

    Commit nombre

---
+++ src/main/java/com/everis/tallergitcolab/TallergitcolabApplication.java
@@ -0,0 +1,22 @@
+package com.everis.tallergitcolab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallergitcolabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallergitcolabApplication.class, args);
+		start();
+	}
+
+	public static void start() {
+		
+		System.out.println("Hello world!");
+		escribeMiNombre();
+	}
+	public static void escribeMiNombre() {
+		System.out.println("David Pineda Hidalgo");
+	}
+}

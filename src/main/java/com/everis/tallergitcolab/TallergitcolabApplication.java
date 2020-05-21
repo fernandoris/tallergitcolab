8343a2538fe3ebb55f6770c8cc9b5e0bf4cb2b74
Francisco José Caro Carazo <fcarocrazo@salesianas.es>
2020-05-21 14:13:44 +0200

    ultimo

--- src/main/java/com/everis/tallergitcolab/TallergitcolabApplication.java
+++ src/main/java/com/everis/tallergitcolab/TallergitcolabApplication.java
@@ -31,6 +31,10 @@ public class TallergitcolabApplication {
 		System.out.println("Alvaro Rueda Sanchez");
 		System.out.println("Irene Rengel");
 		System.out.println("Fernando Vázquez Boza");
+		System.out.println("Samuel Piñero Gomez");
+		System.out.println("Raúl Morales");
+		System.out.println("Luis Rosell Gonzalez");
+		System.out.println("Salvador Muñoz Cordero");
 		System.out.println("Raúl Morales");
 		System.out.println("Luis Rosell Gonzalez");
 		System.out.println("Raúl Morales");

0756f6fd7da24e99a7f5d7cde071c57b63d60431
mcoronal <mcoronal@everis.com>
2020-05-21 14:10:58 +0200

    otro

--- src/main/java/com/everis/tallergitcolab/TallergitcolabApplication.java
+++ src/main/java/com/everis/tallergitcolab/TallergitcolabApplication.java
@@ -29,9 +29,9 @@ public class TallergitcolabApplication {
 		System.out.println("Alvaro Rueda Sanchez");
 		System.out.println("Irene Rengel");
 		System.out.println("Fernando Vázquez Boza");
-
 		System.out.println("Fernando Vázquez Boza");
 		System.out.println("Manuel Corona Lasarte");
+		
 	}
 
 }

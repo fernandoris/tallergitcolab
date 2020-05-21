ba72294d517dc8b5e12424ff728268ab6007dac4
jborregof <joaquin.borrego@a.vedrunasevillasj.es>
2020-05-21 13:24:48 +0200

    no se que he hecho

---
+++ tallergitcolab/src/main/java/com/everis/tallergitcolab/TallergitcolabApplication.java
@@ -0,0 +1,19 @@
+package com.everis.tallergitcolab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallergitcolabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallergitcolabApplication.class, args);
+		start();
+	}
+
+	public static void start() {
+		
+		System.out.println("Hello world!");
+		
+	}
+}

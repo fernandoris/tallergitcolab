9ac4cd523c687ec8e43d3c269d3977575740f3f6
salva <smunozco@everis.com>
2020-05-21 13:43:20 +0200

    Función escribeMiNombre

--- src/main/java/com/everis/tallergitcolab/TallergitcolabApplication.java
+++ src/main/java/com/everis/tallergitcolab/TallergitcolabApplication.java
@@ -33,6 +33,7 @@ public class TallergitcolabApplication {
 		System.out.println("Luis Rosell Gonzalez");
 		System.out.println("Raúl Morales");
 
+		System.out.println("Salvador Muñoz Cordero");
 	}
 
 }

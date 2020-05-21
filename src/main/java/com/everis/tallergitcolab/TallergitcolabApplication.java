5bb0d0ae24b44fe0208561d09676853a2b819c9c
fvazquezb2001 <fvazquezb2001@gmail.com>
2020-05-21 14:49:27 +0200

    intentando no hacerlo mal, mergeo

--- src/main/java/com/everis/tallergitcolab/TallergitcolabApplication.java
+++ src/main/java/com/everis/tallergitcolab/TallergitcolabApplication.java
@@ -20,6 +20,25 @@ public class TallergitcolabApplication {
 	
 	public static void escribeMiNombre(){
 		System.out.println("Fernando Rodríguez-Izquierdo Serrano");		
+		System.out.println("Francisco García Macías");
+		System.out.println("Alvaro Rueda Sanchez");
+		System.out.println("Fernando Vázquez Boza");
+		System.out.println("Raúl Morales");	
+		System.out.println("Francisco José Caro Carazo");		
+		System.out.println("Francisco Javier Carande Cabeza");
+		System.out.println("Yi Chen");	
+		System.out.println("Miguel Alcantara Montero");			
+		System.out.println("Samuel Piñero Gomez");		
+		System.out.println("Irene Rengel");		
+		System.out.println("Alberto Ruiz");	
+		System.out.println("Miguel Alcantara Montero");
+		System.out.println("Luca Gil de Montes de Vries");		
+		System.out.println("Luis Rosell Gonzalez");
+		System.out.println("Salvador Muñoz Cordero");
+		System.out.println("Alicia G L");
+		System.out.println("Manuel Corona Lasarte");
+		System.out.println("Ana Blanco Escudero");
+		System.out.println("Noelia Galindo García");
 	}
 	
 

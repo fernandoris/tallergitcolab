dd313d5a103a92878baa76e0753df2cffad2915c
Yilool <20010917chen@gmail.com>
2020-05-21 14:07:39 +0200

    ResolverYi2

--- src/main/java/com/everis/tallergitcolab/TallergitcolabApplication.java
+++ src/main/java/com/everis/tallergitcolab/TallergitcolabApplication.java
@@ -22,9 +22,6 @@ public class TallergitcolabApplication {
 	
 	public static void escribeMiNombre () {
 		System.out.println("Luca Gil de Montes de Vries");
-	}
-	
-	public static void escribeMiNombre() {
 		System.out.println("Yi Chen");
 		System.out.println("Fernando Rodríguez-Izquierdo Serrano");
 		System.out.println("Miguel Alcantara Montero");
